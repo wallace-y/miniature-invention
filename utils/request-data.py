f0cbf8faff8b89f7129fafcc1d53e2da012ece6f
wallace-y <cameronwfinney@gmail.com>
2023-09-26 16:34:56 +0100

    changing the request data function to be a loop over given dates

--- utils/request-data.py
+++ utils/request-data.py
@@ -1,31 +1,40 @@
 import requests
 import json
 import os
+import sys
+
 from dotenv import load_dotenv
-import utils.format_data as format_data
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_root)
+
+# Now you can import modules from the 'utils' package
+import format_data
 
 # Load the stored environment variables
 load_dotenv()
 
+dates = ["2023-09-25", "2023-09-26"]
+
 api_key = os.getenv("API_KEY")
-date = "2023-09-24"
-url = f"https://api.company-information.service.gov.uk/advanced-search/companies?incorporated_from={date}&incorporated_to={date}&size=5000"
-headers = {"Authorization": f"{api_key}"}
 
 
-def call_CH_api():
-    response = requests.get(url, headers=headers)
-    if response.status_code != 200:
-        print(response.reason)
-    else:
-        data = response.json()
-        if data:
-            json_file = f"./data/{date[:7]}/{date}-data.json"
-            with open(json_file, "w") as json_file:
-                json.dump(data, json_file, indent=4)
+for date in dates:
+    url = f"https://api.company-information.service.gov.uk/advanced-search/companies?incorporated_from={date}&incorporated_to={date}&size=5000"
+    headers = {"Authorization": f"{api_key}"}
+
+    def call_CH_api():
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            print(f"Failed to fetch data for date {date}: {response.reason}")
+        else:
+            data = response.json()
+            if data:
+                json_file = f"./data/{date[:7]}/{date}-data.json"
+                with open(json_file, "w") as json_file:
+                    json.dump(data, json_file, indent=4)
             json_file.close()
-            print("Done")
+            print(f"Data for date {date} saved successfully.")
         format_data.save_json_api_data_to_csv(f"./data/{date[:7]}/{date}-data.json")
 
-
-call_CH_api()
+    call_CH_api()
